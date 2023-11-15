8bad59070e4274a5ed4b0bcba6c3efe911592fcf
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-15 16:35:08 -0300

    Update formativa_s5.py

--- projeto_disciplina/formativa_s5.py
+++ projeto_disciplina/formativa_s5.py
@@ -8,39 +8,24 @@ print('\nBem-vindo(a) ao Sistema PUC!')
 
 while True:
     # sleep(1.5)
-    selecao = 0
-    while True:
-        try:
-            # Menu principal do sistema
-            funcoes.menu_principal()
-
-            selecao = int(input('\n# Digite o número da opção desejada: '))
-            if selecao < 0 or selecao > 5:
-                print('# Opção inválida, Tente novamente.')
-                # sleep(1.5)
-                continue
-            break
-        except ValueError:
-            print('# Valor inválido! Digite apenas valores numéricos.')
-            # sleep(2)
-            continue
+    entrada = funcoes.menu_principal()
+    
     # sleep(1.5)
-
-    if selecao == 0:
+    if entrada == 0:
         print('# Opção selecionada: (0) Sair'
               '\n# Você saiu.')
         break
-    elif selecao == 1:
+    elif entrada == 1:
         print('# Opção selecionada: (1) Estudantes')
-    elif selecao == 2:
+    elif entrada == 2:
         print('# Em DESENVOLVIMENTO...')
         continue
         # print('# Opção selecionada: (2) Disciplinas ')
-    elif selecao == 3:
+    elif entrada == 3:
         print('# Em DESENVOLVIMENTO...')
         continue
         # print('# Opção selecionada: (3) Professores')
-    elif selecao == 4:
+    elif entrada == 4:
         print('# Em DESENVOLVIMENTO...')
         continue
         # print('# Opção selecionada: (4) Turmas ')
@@ -50,56 +35,29 @@ while True:
         # print('# Opção selecionada: (5) Matrículas')
 
     # sleep(1.5)
-    while True:
-        try:
-            print('\n# Menu de operações:'
-                  '\n(1) Incluir'
-                  '\n(2) Listar'
-                  '\n(3) Editar'
-                  '\n(4) Excluir'
-                  '\n(0) Voltar para o menu principal')
-            selecao = int(input('\n# Digite o número da opção desejada: '))
-            if selecao < 0 or selecao > 4:
-                print('# Opção inválida! Tente novamente.')
-                continue
-            break
-        except ValueError:
-            print('# Valor inválido! Digite apenas valores numéricos.')
-            # sleep(2)
-            continue
+    entrada = funcoes.menu_operacoes()
 
     # sleep(1.5)
 
     # VOLTAR AO MENU PRINCIPAL
-    if selecao == 0:
+    if entrada == 0:
         print('# Opção selecionada: Voltar ao menu principal')
         continue
 
     # ADICIONAR ESTUDANTES NA LISTA
-    elif selecao == 1:
-        funcoes.incluir_estudante(alunos)
+    elif entrada == 1:
+        alunos = funcoes.incluir_estudante(alunos)
 
     # MOSTRAR LISTA DE ESTUDANTES
-    elif selecao == 2:
-        print('# Opção selecionada: 2. Listar')
-        if len(alunos) > 0:
-            print(f'\nLista de alunos:')
-            for aluno in alunos:
-                print(f'- {aluno}')
-        else:
-            print('# Não há estudantes cadastrados :(')
+    elif entrada == 2:
+        alunos = funcoes.mostrar_lista_estudantes(alunos)
 
     # EDITAR INFORMAÇÕES DE ESTUDANTE
-    elif selecao == 3:
+    elif entrada == 3:
         print('\n# Opção selecionada: 2. Editar')
 
         # MOSTRA LISTA DE ESTUDANTES
-        if len(alunos) > 0:
-            print(f'\nLista de alunos:')
-            for aluno in alunos:
-                print(f'- {aluno}')
-        else:
-            print('# Não há estudantes cadastrados :(')
+        funcoes.mostrar_lista_estudantes(alunos)
 
         entrada = int(input('\n# Digite o código do estudante para editar as informações: '))
         print('Estudante selecionado!')
@@ -129,7 +87,7 @@ while True:
                 pass
 
     # EXCLUIR ESTUDANTE
-    else:
+    elif entrada == 4:
         if len(alunos) > 0:
             print(f'\nLista de alunos:')
             for aluno in alunos:
