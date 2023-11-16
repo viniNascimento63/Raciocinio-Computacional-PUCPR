4ad445be526777859ea8022bddf6b6f7e3b29cd0
Vinícius Nascimento <vini_sn@outlook.com>
2023-11-16 13:06:07 -0300

    Otimizando através de funções

--- projeto_disciplina/formativa_s5.py
+++ projeto_disciplina/formativa_s5.py
@@ -59,32 +59,8 @@ while True:
         # MOSTRA LISTA DE ESTUDANTES
         funcoes.mostrar_lista_estudantes(alunos)
 
-        entrada = int(input('\n# Digite o código do estudante para editar as informações: '))
-        print('Estudante selecionado!')
-
-        for indice, dicionario in enumerate(alunos):
-
-            # Verifica se a entrada é uma chave no dicionário atual
-            if entrada in dicionario.values():
-
-                # Recebe as novas informações do estudante
-                codigo = int(input('\n# Atualize o código do estudante: '))
-                nome = input('# Digite o nome do estudante: ')
-                cpf = int(input('# Digite o CPF do estudante (sem pontos ou espaços): '))
-
-                # Um novo dicionario recebe as informações atualizadas
-                estudante = {
-                    "Código": codigo,
-                    "Nome": nome,
-                    "CPF": cpf
-                }
-
-                # Substitui os dados antigos do estudante pelos novos
-                alunos[indice] = estudante
-
-                print(f'# Informações de {dicionario["Nome"]} editadas com sucesso!')
-            else:
-                pass
+        # EDITA INFORMAÇÕES DO ESTUDANTE
+        alunos = funcoes.editar_informacao_aluno(alunos)
 
     # EXCLUIR ESTUDANTE
     elif entrada == 4:
